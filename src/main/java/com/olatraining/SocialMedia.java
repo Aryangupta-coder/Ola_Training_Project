3d63584f4b2f79f6fe35a14cda650abf75100130
Aryan Gupta2 <aryangupta0382000@gmail.com>
2022-07-27 15:01:49 +0530

    Initial Commit

---
+++ src/main/java/com/olatraining/SocialMedia.java
@@ -0,0 +1,11 @@
+package com.olatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMedia {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMedia.class,args);
+    }
+}
