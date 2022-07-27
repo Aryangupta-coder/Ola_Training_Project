4f9259cfd2e271f2d0adac019dafd42a000d5e5d
Aryan Gupta2 <aryangupta0382000@gmail.com>
2022-07-27 15:56:40 +0530

    Added Sign up Functionality

--- src/main/java/com/olatraining/controller/UserController.java
+++ src/main/java/com/olatraining/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.olatraining.controller;
 
+
+import com.olatraining.model.Response;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.olatraining.model.SignUpRequest;
+
+@RestController
 public class UserController {
+
+    @PostMapping(value = "/signUp",produces ="application/json", consumes = "application/json")
+    public Response signUp(@RequestBody SignUpRequest signUpRequest){
+        Response signUpResponse = new Response();
+        if(signUpRequest.getEmail().isEmpty()== false && signUpRequest.getId()!= 0 && signUpRequest.getPassword().isEmpty()== false
+        && signUpRequest.getPhoneNo().isEmpty()== false){
+            signUpResponse.setMessage("Sign up Successful");
+            signUpResponse.setStatus(true);
+
+        }else{
+            signUpResponse.setMessage("Sign up Failed as all field needs to be filles");
+            signUpResponse.setStatus(false);
+        }
+        return signUpResponse;
+    }
 }
